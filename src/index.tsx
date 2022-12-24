eeeb9cec0a29b3c8c2d1938daa6a61da3a5b1ae8
valera <traberker901@gmail.com>
2022-12-24 23:23:46 +0300

    bugFix: replace BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,19 @@
-import React from "react"
+import { createRoot } from "react-dom/client"
+import { Provider } from "react-redux"
+import { HashRouter } from "react-router-dom"
+import App from "./app/App"
+import { store } from "./app/store"
 import "./index.css"
 import reportWebVitals from "./reportWebVitals"
-import App from "./app/App"
-import { createRoot } from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
-import { store } from "./app/store"
-import { Provider } from "react-redux"
 
 const container = document.getElementById("root")
 const root = createRoot(container!)
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 )
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
